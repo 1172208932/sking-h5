cb382330449cee92b78fa97ca0d2624caa063156
张晓旭 <zhangxiaoxu@duiba.com.cn>
2022-01-13 11:05:10 +0800

    feat: rolecontrol 格式化

--- src/pages/gamepage/mixins/roleControl.js
+++ src/pages/gamepage/mixins/roleControl.js
@@ -5,70 +5,70 @@ import Role from '../components/Role'
 import p2 from 'p2/build/p2';
 
 export const RoleControl = {
-	role:null,
-	// 双击
-	timer:null,
-	count:null,
-	ifFly:false,
+    role: null,
+    // 双击
+    timer: null,
+    count: null,
+    ifFly: false,
 
-	addRole(){
-		this.role = new Role()
-		this.bgCon.addChild(this.role)
+    addRole() {
+        this.role = new Role()
+        this.bgCon.addChild(this.role)
 
-		this.addMaterial(this.heighshape)
-		
-		this.phyworld.addBody(this.role.circleBody);
-		this.phyworld.addBody(this.role.circleBody2);
-		this.phyworld.addBody(this.role.carBody);
-		let revoluteBack = new p2.LockConstraint(this.role.carBody, this.role.circleBody);
-		let revoluteFront = new p2.LockConstraint(this.role.carBody, this.role.circleBody2);
-		this.phyworld.addConstraint(revoluteBack);
-		this.phyworld.addConstraint(revoluteFront);
-	},
-    addMaterial(heighshape){
-		var contactMaterial1  = new p2.ContactMaterial(heighshape.material,this.role.circleShape.material, {
-			restitution: 0, // This means no bounce!
-			surfaceVelocity: -82000,
-			friction: 0.8
-		});
-		this.phyworld.addContactMaterial(contactMaterial1)
+        this.addMaterial(this.heighshape)
 
-		var contactMaterial2 =new p2.ContactMaterial(heighshape.material,this.role.circleShape2.material, {
-			restitution: 0, // This means no bounce!
-			surfaceVelocity: -82000,
-			friction: 0.8
-		});
-		this.phyworld.addContactMaterial(contactMaterial2)
+        this.phyworld.addBody(this.role.circleBody);
+        this.phyworld.addBody(this.role.circleBody2);
+        this.phyworld.addBody(this.role.carBody);
+        let revoluteBack = new p2.LockConstraint(this.role.carBody, this.role.circleBody);
+        let revoluteFront = new p2.LockConstraint(this.role.carBody, this.role.circleBody2);
+        this.phyworld.addConstraint(revoluteBack);
+        this.phyworld.addConstraint(revoluteFront);
+    },
+    addMaterial(heighshape) {
+        var contactMaterial1 = new p2.ContactMaterial(heighshape.material, this.role.circleShape.material, {
+            restitution: 0, // This means no bounce!
+            surfaceVelocity: -82000,
+            friction: 0.8
+        });
+        this.phyworld.addContactMaterial(contactMaterial1)
 
-		var contactMaterial3  = new p2.ContactMaterial(heighshape.material,this.role.carShape.material, {
-			restitution: 0, // This means no bounce!
-			surfaceVelocity: -82000,
-			friction: 0
-		});
-		this.phyworld.addContactMaterial(contactMaterial3)
-	},
-    updateRole(stage){
+        var contactMaterial2 = new p2.ContactMaterial(heighshape.material, this.role.circleShape2.material, {
+            restitution: 0, // This means no bounce!
+            surfaceVelocity: -82000,
+            friction: 0.8
+        });
+        this.phyworld.addContactMaterial(contactMaterial2)
+
+        var contactMaterial3 = new p2.ContactMaterial(heighshape.material, this.role.carShape.material, {
+            restitution: 0, // This means no bounce!
+            surfaceVelocity: -82000,
+            friction: 0
+        });
+        this.phyworld.addContactMaterial(contactMaterial3)
+    },
+    updateRole(stage) {
         const x = this.role.circleBody.position[0];
-		const y = -this.role.circleBody.position[1];
+        const y = -this.role.circleBody.position[1];
 
-		const carBodyX = this.role.carBody.position[0];
-		const carBodyY = -this.role.carBody.position[1];
+        const carBodyX = this.role.carBody.position[0];
+        const carBodyY = -this.role.carBody.position[1];
 
-		const circleBody2X = this.role.circleBody2.position[0];
-		const circleBody2Y = -this.role.circleBody2.position[1];
+        const circleBody2X = this.role.circleBody2.position[0];
+        const circleBody2Y = -this.role.circleBody2.position[1];
 
 
-		this.role.circle2.position.set(circleBody2X, circleBody2Y);
-		this.role.car.position.set(carBodyX - 40, carBodyY);
+        this.role.circle2.position.set(circleBody2X, circleBody2Y);
+        this.role.car.position.set(carBodyX - 40, carBodyY);
 
         this.role.circle.position.set(x, y);
         this.role.car.rotation = -this.role.carBody.angle / Math.PI * 180
 
-        this.bgCon.x = -x + stage.width / 4   //镜头跟随
-        this.bgCon.y = -y + stage.height *0.6
+        this.bgCon.x = -x + stage.width / 4 //镜头跟随
+        this.bgCon.y = -y + stage.height * 0.6
     },
 
-    roleContact(e){
+    roleContact(e) {
         if (
             (e.bodyA.id == this.role.circleBody.id && e.bodyB.id == this.line0.id) ||
             (e.bodyB.id == this.role.circleBody.id && e.bodyA.id == this.line0.id) ||
