8bdb7f8f7c0deddfb690d18af8ce4cfd1d261395
xiaojudan <944538864@qq.com>
2022-01-21 15:45:59 +0800

    修改21关提醒弹窗的开始闯关

--- src/panel/gameRemind/gameRemind.jsx
+++ src/panel/gameRemind/gameRemind.jsx
@@ -8,6 +8,7 @@ import modalStore from '@src/store/modal';
 import API from '../../api';
 import './gameRemind.less';
 import { _throttle } from '@src/utils/utils.js';
+import { hideLoading, showLoading, Toast } from '@spark/ui';
 
 @observer
 class GameRemind extends React.Component {
@@ -17,9 +18,31 @@ class GameRemind extends React.Component {
   clickStart = _throttle(async() => {
     const {popData} = this.props;
     modalStore.closePop("GameRemind");
-    await store.getHomeInfo();
-    store.startGame(popData.level);
+    this.startGame(popData.level);
   })
+
+  // 开始游戏
+  async startGame(level) {
+    if (store.homeInfo?.joinGolds > store.homeInfo?.goldNum) {
+      modalStore.pushPop("NoMoney");
+    } else {
+      modalStore.pushPop("PayConfirm",{
+        needCoin:store.homeInfo.joinGolds,
+        start:async() => {
+          store.setCurrentGameLevel(level);
+          modalStore.closePop("PayConfirm")
+          showLoading()
+          const {success,data} = await API.startGame()
+          hideLoading()
+          if(success && data) {
+            Toast("金币-"+store.homeInfo?.joinGolds||0);
+            store.setStartId(data)
+            store.changePage("Gamepage");
+          }
+        }
+      })
+    }
+  }
   render() {
     const {homeInfo} = store
     return (
