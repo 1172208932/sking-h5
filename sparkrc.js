6b3056267f588bbf0ddb7613fb1329109c85a781
xiaojudan <944538864@qq.com>
2022-01-24 16:01:45 +0800

    修改关闭地图页弹窗导致滑动位移问题

--- sparkrc.js
+++ sparkrc.js
@@ -10,5 +10,5 @@ module.exports ={
   "IMAGE_Q2": 0.8,
   "RES_PATH": "/src/assets/",
   "RES_PATH_PROD": "//yun.duiba.com.cn/spark/v2/temp_base/1643005478227",
-  "JS_PATH_PROD": "https://yun.duiba.com.cn/spark/v2/temp_base/1643005650283/js"
+  "JS_PATH_PROD": "https://yun.duiba.com.cn/spark/v2/temp_base/1643007822704/js"
 }
