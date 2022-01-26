8d90c33807fa91bceabe67c026f4717811c3cc77
xiaojudan <944538864@qq.com>
2022-01-26 19:43:25 +0800

    修改提醒弹窗

--- sparkrc.js
+++ sparkrc.js
@@ -10,5 +10,5 @@ module.exports ={
   "IMAGE_Q2": 0.8,
   "RES_PATH": "/src/assets/",
   "RES_PATH_PROD": "//yun.duiba.com.cn/spark/v2/temp_base/1643169142513",
-  "JS_PATH_PROD": "https://yun.duiba.com.cn/spark/v2/temp_base/1643183771335/js"
+  "JS_PATH_PROD": "https://yun.duiba.com.cn/spark/v2/temp_base/1643184020879/js"
 }
