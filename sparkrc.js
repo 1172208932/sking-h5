3126026c7d51ecbb3dfe2ee84581fd2342540734
xiaojudan <944538864@qq.com>
2022-01-28 18:10:53 +0800

    打包

--- sparkrc.js
+++ sparkrc.js
@@ -10,5 +10,5 @@ module.exports ={
   "IMAGE_Q2": 0.8,
   "RES_PATH": "/src/assets/",
   "RES_PATH_PROD": "//yun.duiba.com.cn/spark/v2/temp_base/1643349868556",
-  "JS_PATH_PROD": "https://yun.duiba.com.cn/spark/v2/temp_base/1643350019412/js"
+  "JS_PATH_PROD": "https://yun.duiba.com.cn/spark/v2/temp_base/1643355990442/js"
 }
