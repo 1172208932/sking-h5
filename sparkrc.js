acfbc56cd3ab6e51e3ee388aff4dce60ed1f795f
wangzhujun <wangzhujun@duiba.com.cn>
2022-02-08 14:27:38 +0800

    94

--- sparkrc.js
+++ sparkrc.js
@@ -10,5 +10,5 @@ module.exports ={
   "IMAGE_Q2": 0.8,
   "RES_PATH": "/src/assets/",
   "RES_PATH_PROD": "//yun.duiba.com.cn/spark/v2/temp_base/1644286302265",
-  "JS_PATH_PROD": "https://yun.duiba.com.cn/spark/v2/temp_base/1644300195958/js"
+  "JS_PATH_PROD": "https://yun.duiba.com.cn/spark/v2/temp_base/1644300123706/js"
 }
